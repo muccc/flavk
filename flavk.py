e74cd0e78d6c6ce673c8a7ffb56ab4885e14bbcd
Markus <john.subscriber@markus.institute>
2018-09-22 22:11:22 +0000

    Add controls for projector

--- flavk.py
+++ flavk.py
@@ -7,6 +7,7 @@ from flask import Flask, request, render_template, redirect
 from time import sleep
 
 pioneer = ("83.133.178.82", 23)
+optoma = ("83.133.178.103", 1023)
 
 setupCommands = {
     "tisch": ("04FN",),
@@ -21,34 +22,75 @@ lautsprecherOptions = {
     "sued": "2",
 }
 
+projectorCommands = {
+    "on": "~0000 1",
+    "off": "~0000 0",
+    "power?": "~00124 1",
+}
+
+projectorSetupCommands = {
+    "receiver": ("~0012 1",),
+    "vga": ("~0012 13",)
+}
+
 app = Flask(__name__)
 
 def sendCommand(command, session):
     session.write(command.encode('ascii') + b"\r")
 
-def getSession(target = pioneer):
+def getSession(target):
     return t.Telnet(target[0],target[1])
 
-def powerOn(session):
+def getReceiverSession():
+    return getSession(pioneer)
+
+def getProjectorSession():
+    return getSession(optoma)
+
+def powerOnReceiver(session):
     sendCommand("PO", session)
 
-def powerOff(session):
+def powerOffReceiver(session):
     sendCommand("PF", session)
 
-def getPowerStatus(session):
+def powerOnProjector(session):
+    sendCommand(projectorCommands["on"], session)
+
+def powerOffProjector(session):
+    sendCommand(projectorCommands["off"], session)
+
+def getReceiverPowerStatus(session):
     session.read_eager()
     sendCommand("?P", session)
     return "PWR0" in session.read_some().decode('ascii')
 
-def setUp():
-    s = getSession()
-    on = getPowerStatus(s)
+def getProjectorPowerStatus(session):
+    sendCommand(projectorCommands["power?"], session)
+    response = session.read_some()
+    return "Ok1" in response.decode('ascii')
+
+def setUpReceiver():
+    s = getReceiverSession()
+    on = getReceiverPowerStatus(s)
 
     if not on:
-        powerOn(s)
+        powerOnReceiver(s)
         sleep(0.1)
         sendCommand(lautsprecherOptions["alle"]+"SPK",s)
 
+    return s
+
+def setUpProjector():
+    s = getProjectorSession()
+
+    ## Multiple commands seem to be a problem with the projector
+    #on = getProjectorPowerStatus(s)
+    on = False
+
+    if not on:
+        sleep(0.3)
+        powerOnProjector(s)
+    sleep(0.3)
     return s
 
 def renderedIndex():
@@ -63,7 +105,7 @@ def index():
 
 @app.route("/receiver/setups/<setting>")
 def setups(setting):
-    s = setUp()
+    s = setUpReceiver()
 
     if setting in setupCommands.keys():
         for command in setupCommands[setting]:
@@ -73,19 +115,38 @@ def setups(setting):
 @app.route("/receiver/power/<state>")
 def power(state):
     if state == "on":
-        s = setUp()
+        s = setUpReceiver()
     elif state == "off":
-        s = getSession()
-        powerOff(s)
+        s = getReceiverSession()
+        sleep(0.3)
+        powerOffReceiver(s)
     return redirect("/")
 
 @app.route("/receiver/lautsprecher")
 def lautsprecher():
-    s = setUp()
+    s = setUpReceiver()
 
     choice = request.args.get('welche')
     if choice in lautsprecherOptions.keys():
         sendCommand(lautsprecherOptions[choice]+"SPK",s)
+    return redirect("/")
+
+@app.route("/projector/power/<state>")
+def projectorPower(state):
+    if state == "on":
+        s = setUpProjector()
+    elif state == "off":
+        s = getProjectorSession()
+        powerOffProjector(s)
+    return redirect("/")
+
+@app.route("/projector/setups/<setting>")
+def projectorSetups(setting):
+    s = setUpProjector()
+
+    if setting in projectorSetupCommands.keys():
+        for command in projectorSetupCommands[setting]:
+            sendCommand(command, s)
     return redirect("/")
 
 if __name__ == "__main__":
